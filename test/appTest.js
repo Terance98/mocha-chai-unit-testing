a86c29a275c69e1195ad8aee1661089eededf887
terance98 <thomasterance98@gmail.com>
2020-03-23 14:29:38 +0530

    Mutliple tests

--- test/appTest.js
+++ test/appTest.js
@@ -2,9 +2,24 @@ const assert = require('chai').assert;
 
 const app = require('../app');
 
+const sayHelloResult = app.sayHello();
+const addNumbersResult = app.addNumbers(5,5);
+
 
 describe('App', function(){
-    it('app should return hello', function(){
-        assert.equal(app(), "hello");
+    it('sayHello should return hello', function(){
+        assert.equal(sayHelloResult, "hello");
+    });
+
+    it('sayHello should return type String', function(){
+        assert.typeOf(sayHelloResult, 'string');
+    });
+
+    it('addNumbers should be above 5', function(){
+        assert.isAbove(addNumbersResult, 5);
+    });
+
+    it('addNumbers should return type Number', function(){
+        assert.typeOf(addNumbersResult, 'number');
     });
 });
